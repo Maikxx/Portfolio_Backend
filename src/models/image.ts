cd16c75cababee5c92d72298df3e74cddf75e5a3
Maikel van Veen <maikeloo10@hotmail.com>
2018-02-17 20:36:25 +0100

    feat(*): improve typing

--- src/models/image.ts
+++ src/models/image.ts
@@ -4,8 +4,29 @@ import * as mongoose from 'mongoose';
 const imageScheme = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     image: {
-        type: String,
+        type: Object,
         required: true,
+
+        originalname: {
+            type: String,
+            required: true,
+        },
+        destination: {
+            type: String,
+            required: true,
+        },
+        filename: {
+            type: String,
+            required: true,
+        },
+        path: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
     },
     name: String,
     description: String,
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -2,17 +2,26 @@ import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as multer from 'multer';
 
+interface FileType {
+    mimetype: string;
+    originalname: string;
+    destination: string;
+    filename: string;
+    path: string;
+    size: string;
+}
+
 const storage = multer.diskStorage({
-    destination: (request, file, callback) => {
+    destination: (request: object, file: FileType, callback: any) => {
         callback(null, 'uploads/');
     },
-    filename: (request, file, callback) => {
+    filename: (request: object, file: FileType, callback: any) => {
         // tslint:disable-next-line:prefer-template
         callback(null, new Date().toISOString() + '-' + file.originalname);
     },
 });
 
-const fileFilter = (request, file, callback) => {
+const fileFilter = (request: object, file: FileType, callback: any) => {
     if (file.mimetype === 'image/jpeg' ||
         file.mimetype === 'image/jpg' ||
         file.mimetype === 'image/png') {
@@ -31,13 +40,24 @@ const Image = require('../models/image');
 const router = express.Router();
 
 // Create a new image with the name, description and location that are passed in via the request.
-router.post('/', upload.single('image'), (req, res, next) => {
+router.post('/', upload.single('image'), (req: any, res: any, next: any) => {
+
+    function calculateFileSize(size: number) {
+        return size / 1000;
+    }
+
     const image = new Image({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         description: req.body.description,
         location: req.body.location,
-        image: (req as any).file.path,
+        image: {
+            originalname: req.file.originalname,
+            destination: req.file.destination,
+            filename: req.file.filename,
+            path: req.file.path,
+            size: `${calculateFileSize(req.file.size)}kb`,
+        },
     });
 
     const serverURL = `${req.protocol}://${req.get('host')}`;
@@ -65,7 +85,7 @@ router.post('/', upload.single('image'), (req, res, next) => {
 
 // Get all the Images from the database.
 // Select does what it describes, it selects items to return in the response of the find.
-router.get('/', (req, res, next) => {
+router.get('/', (req: any, res: any, next: any) => {
     const serverURL = `${req.protocol}${req.get('host')}`;
 
     Image.find()
@@ -93,7 +113,7 @@ router.get('/', (req, res, next) => {
 });
 
 // Get a single Image by passing the id into the URL.
-router.get('/:imageId', (req, res, next) => {
+router.get('/:imageId', (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
     const serverURL = `${req.protocol}${req.get('host')}`;
 
@@ -126,7 +146,7 @@ router.get('/:imageId', (req, res, next) => {
 // If you want to access this, set a body of a request, with an array of objects.
 // Each object should have a propName assigned to the corresponding propName that you want to update, and a "value".
 // The value you set here is the new value for the property.
-router.patch('/:imageId', (req, res, next) => {
+router.patch('/:imageId', (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
     const updateOps = {};
     const serverURL = `${req.protocol}${req.get('host')}`;
@@ -160,7 +180,7 @@ router.patch('/:imageId', (req, res, next) => {
 });
 
 // Delete all database logs of type Image.
-router.delete('/', (req, res, next) => {
+router.delete('/', (req: any, res: any, next: any) => {
     const serverURL = `${req.protocol}${req.get('host')}`;
 
     Image.remove({})
@@ -179,7 +199,7 @@ router.delete('/', (req, res, next) => {
 });
 
 // Delete only the Image with the id passed in to the URL.
-router.delete('/:imageId', (req, res, next) => {
+router.delete('/:imageId', (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
     const serverURL = `${req.protocol}${req.get('host')}`;
 
@@ -201,7 +221,7 @@ router.delete('/:imageId', (req, res, next) => {
 });
 
 function onError (response, error) {
-    console.log(error);
+    console.error(error);
 
     response.status(500).json({
         error: error,
