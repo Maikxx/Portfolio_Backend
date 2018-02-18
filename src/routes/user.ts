fdf32face077821727806f068749ac638a87054b
Maikel van Veen <maikeloo10@hotmail.com>
2018-02-18 15:20:49 +0100

    feat(user): add JWT for getting users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
 
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req: any, res: any, next: any) => {
@@ -43,6 +44,46 @@ router.post('/signup', (req: any, res: any, next: any) => {
                 });
             }
         });
+});
+
+// Route for login in a user.
+router.post('/login', (req: any, res: any, next: any) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Authentication failed',
+                });
+            }
+
+            bcrypt.compare(req.body.password, user[0].password, (error, response) => {
+                if (error) {
+                    return res.status(401).json({
+                        message: 'Authentication failed',
+                    });
+                }
+
+                if (response) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id,
+                    }, process.env.JWT_KEY, {
+                        expiresIn: '1h',
+                    });
+
+                    return res.status(200).json({
+                        message: 'Authentication successful',
+                        token: token,
+                    });
+                } else {
+                    return res.status(401).json({
+                        message: 'Authentication failed',
+                    });
+                }
+            });
+        })
+        .catch(error => onError(res, error));
 });
 
 // DANGER: Delete all current users.
