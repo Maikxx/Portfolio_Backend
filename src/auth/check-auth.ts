08efcd32cc85002a262bc653d19c697564c9e67e
Maikel van Veen <maikeloo10@hotmail.com>
2018-02-18 17:30:49 +0100

    feat(auth): add authentication and a big refactor

---
+++ src/auth/check-auth.ts
@@ -0,0 +1,16 @@
+import * as jwt from 'jsonwebtoken';
+
+module.exports = (req: any, res: any, next: any) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.userDate = decoded;
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            message: 'Auth failed',
+        });
+    }
+};
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,6 +1,10 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as multer from 'multer';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const checkAuth = require('../auth/check-auth');
 
 interface FileType {
     mimetype: string;
@@ -9,6 +13,14 @@ interface FileType {
     filename: string;
     path: string;
     size: string;
+}
+
+interface ImageType {
+    _id: string;
+    name: string;
+    description: string;
+    location: string;
+    image: any;
 }
 
 const storage = multer.diskStorage({
@@ -30,7 +42,7 @@ const fileFilter = (request: object, file: FileType, callback: any) => {
     } else {
         callback(null, false);
     }
-}
+};
 
 const upload = multer({
     storage: storage,
@@ -41,8 +53,7 @@ const Image = require('../models/image');
 const router = express.Router();
 
 // Create a new image with the name, description and location that are passed in via the request.
-router.post('/', upload.single('image'), (req: any, res: any, next: any) => {
-
+router.post('/', upload.single('image'), checkAuth, (req: any, res: any, next: any) => {
     function calculateFileSize(size: number) {
         return size / 1000;
     }
@@ -61,65 +72,84 @@ router.post('/', upload.single('image'), (req: any, res: any, next: any) => {
         },
     });
 
-    const serverURL = `${req.protocol}://${req.get('host')}`;
-
     image.save()
-        .then(result => {
+        .then((result: ImageType) => {
+            const {
+                _id,
+                name,
+                description,
+                location,
+                image,
+            } = result;
+
             res.status(201).json({
                 storedImage: {
-                    _id: result._id,
-                    name: result.name,
-                    description: result.description,
-                    location: result.location,
-                    image: result.image,
+                    _id: _id,
+                    name: name,
+                    description: description,
+                    location: location,
+                    image: image,
                     requestUsed: 'POST',
                     requestThisImage: {
                         type: 'GET',
                         description: 'Get the last saved image via this request.',
-                        url: getSingleImageUrl(serverURL, result),
+                        url: getSingleImageUrl(generateServerUrl(req), _id),
                     },
                 },
             });
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 // Get all the Images from the database.
 // Select does what it describes, it selects items to return in the response of the find.
-router.get('/', (req: any, res: any, next: any) => {
-    const serverURL = `${req.protocol}${req.get('host')}`;
-
+router.get('/', checkAuth, (req: any, res: any, next: any) => {
     Image.find()
         .select('name description location image')
         .exec()
-        .then(results => {
+        .then((results: any) => {
             res.status(200).json({
                 count: results.length,
-                images: results.map(result => {
+                images: results.map((result: ImageType) => {
+                    const {
+                        _id,
+                        name,
+                        description,
+                        location,
+                        image,
+                    } = result;
+
                     return {
-                        _id: result._id,
-                        name: result.name,
-                        description: result.description,
-                        location: result.location,
-                        image: result.image,
+                        _id: _id,
+                        name: name,
+                        description: description,
+                        location: location,
+                        image: image,
                         requestUsed: {
                             type: 'GET',
-                            url: getSingleImageUrl(serverURL, result),
+                            url: getSingleImageUrl(generateServerUrl(req), _id),
                         },
                     };
                 }),
             });
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 // Get a single Image by passing the id into the URL.
-router.get('/:imageId', (req: any, res: any, next: any) => {
+router.get('/:imageId', checkAuth, (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
-    const serverURL = `${req.protocol}${req.get('host')}`;
 
     Image.findById(imageId)
-        .then(result => {
+        .then((result: ImageType) => {
+            const {
+                _id,
+                name,
+                description,
+                location,
+                image,
+            } = result;
+
             if (!result) {
                 res.status(404).json({
                     message: 'No valid entry found for the provided ID',
@@ -127,96 +157,116 @@ router.get('/:imageId', (req: any, res: any, next: any) => {
             } else {
                 res.status(200).json({
                     storedImage: {
-                        _id: result._id,
-                        name: result.name,
-                        description: result.description,
-                        location: result.location,
-                        image: result.image,
+                        _id: _id,
+                        name: name,
+                        description: description,
+                        location: location,
+                        image: image,
                         requestUsed: {
                             type: 'GET',
-                            url: getSingleImageUrl(serverURL, result),
+                            url: getSingleImageUrl(generateServerUrl(req), _id),
                         },
                     },
                 });
             }
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 // Update a single image handling route.
 // If you want to access this, set a body of a request, with an array of objects.
 // Each object should have a propName assigned to the corresponding propName that you want to update, and a "value".
 // The value you set here is the new value for the property.
-router.patch('/:imageId', (req: any, res: any, next: any) => {
+router.patch('/:imageId', checkAuth, (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
     const updateOps = {};
-    const serverURL = `${req.protocol}${req.get('host')}`;
 
     for (const ops of req.params.imageId) {
         updateOps[ops.propName] = ops.value;
     }
 
     Image.update({ _id: imageId }, { $set: updateOps })
-        .then(result => {
+        .then((result: ImageType) => {
+            const {
+                _id,
+                name,
+                description,
+                location,
+                image,
+            } = result;
+
             res.status(200).json({
                 patchedImage: {
-                    _id: result._id,
-                    name: result.name,
-                    description: result.description,
-                    location: result.location,
-                    image: result.image,
+                    _id: _id,
+                    name: name,
+                    description: description,
+                    location: location,
+                    image: image,
                     requestUsed: {
                         type: 'PATCH',
-                        url: getSingleImageUrl(serverURL, result),
+                        url: getSingleImageUrl(generateServerUrl(req), _id),
                     },
                     requestThisImage: {
                         type: 'GET',
                         description: 'Get the patched (changed) image via this request',
-                        url: getSingleImageUrl(serverURL, result),
+                        url: getSingleImageUrl(generateServerUrl(req), _id),
                     },
                 },
             });
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 // Delete all database logs of type Image.
-router.delete('/', (req: any, res: any, next: any) => {
-    const serverURL = `${req.protocol}${req.get('host')}`;
-
+router.delete('/', checkAuth, (req: any, res: any, next: any) => {
     Image.remove({})
         .exec()
-        .then(result => {
+        .then((result: object) => {
+            const dirToRemoveFrom = 'uploads';
+
+            fs.readdir(dirToRemoveFrom, (error: any, files: any) => {
+                if (error) {
+                    return console.error(error);
+                }
+
+                for (const file of files) {
+                    fs.unlink(path.join(dirToRemoveFrom, file), (error: any) => {
+                        if (typeof error !== null) {
+                            return console.error(error, 'hihi');
+                        }
+                    });
+                }
+            });
+
             res.status(200).json({
                 message: 'Deleted all items',
                 requestAllImages: {
                     type: 'GET',
                     description: 'See the empty list of images.',
-                    url: getAllImages(serverURL),
+                    url: getAllImages(generateServerUrl(req)),
                 },
             });
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 // Delete only the Image with the id passed in to the URL.
-router.delete('/:imageId', (req: any, res: any, next: any) => {
+router.delete('/:imageId', checkAuth, (req: any, res: any, next: any) => {
     const imageId = req.params.imageId;
-    const serverURL = `${req.protocol}${req.get('host')}`;
 
     Image.remove({ _id: imageId })
         .exec()
-        .then(result => {
+        .then((result: object) => {
             res.status(200).json({
                 message: 'Deleted one item',
                 requestAllImages: {
                     type: 'GET',
                     description: 'See the list of images that remained after this deletion.',
-                    url: getAllImages(serverURL),
+                    url: getAllImages(generateServerUrl(req)),
                 },
             });
         })
-        .catch(error => onError(res, error));
+        .catch((error: any) => onError(res, error));
 });
 
 function onError (response: any, error: any) {
@@ -227,8 +277,12 @@ function onError (response: any, error: any) {
     });
 }
 
-function getSingleImageUrl (serverURL: string, imageResult: { _id: string }) {
-    return `${serverURL}/api/images/${imageResult._id}`;
+function generateServerUrl (req: any) {
+    return `${req.protocol}://${req.get('host')}`;
+}
+
+function getSingleImageUrl (serverURL: string, imageId: string) {
+    return `${serverURL}/api/images/${imageId}`;
 }
 
 function getAllImages (serverURL: string) {
