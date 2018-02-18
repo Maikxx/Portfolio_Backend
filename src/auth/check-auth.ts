3021266f0c813e0b81ec193a26c7925a2b959a44
Maikel van Veen <maikeloo10@hotmail.com>
2018-02-18 17:41:05 +0100

    fix(*): fix several specifity items

--- src/auth/check-auth.ts
+++ src/auth/check-auth.ts
@@ -2,13 +2,14 @@ import * as jwt from 'jsonwebtoken';
 
 module.exports = (req: any, res: any, next: any) => {
     try {
+        // Get the token from the client via headers and remove the Bearer piece of it.
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userDate = decoded;
+        req.userData = decoded;
 
         next();
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(401).json({
             message: 'Authentication failed',
         });
